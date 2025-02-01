98afe10fbdadda0443431efa79656768b5405c17
clashv2ray-hub <153529556+clashv2ray-hub@users.noreply.github.com>
2025-02-01 19:00:22 +0800

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// Function to recursively get all files in a directory
+// Function to recursively get all files in a directory, excluding .git and .github
 function getAllFiles(dirPath, arrayOfFiles) {
     const files = fs.readdirSync(dirPath);
 
     arrayOfFiles = arrayOfFiles || [];
 
     files.forEach(function(file) {
+        if (['.git', '.github'].includes(file)) {
+            return;
+        }
         if (fs.statSync(dirPath + '/' + file).isDirectory()) {
             arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles);
         } else {
